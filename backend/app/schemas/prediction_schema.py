9889a0ca3e392fcb3b79d5112c5659c0fd42ae6e
Ankit-bit-cyber <208ankitkumarssm@gmail.com>
2026-02-23 17:05:54 +0530

    chore: Added Schema

--- backend/app/schemas/prediction_schema.py
+++ backend/app/schemas/prediction_schema.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class PredictionRequest(BaseModel):
-    Gender: str
-    ScheduledDay: datetime
-    AppointmentDay: datetime
-    Age: int
-    Neighbourhood: str
-    Scholarship: int
-    Hipertension: int
-    Diabetes: int
-    Alcoholism: int
-    Handcap: int
-    SMS_received: int
+    Gender: str
+    ScheduledDay: datetime
+    AppointmentDay: datetime
+    Age: int
+    Neighbourhood: str
+    Scholoarship: int
+    Hypertension: int
+    Diabetes: int
+    Alcoholism: int
+    Handcap: int
+    SMS_received: int
