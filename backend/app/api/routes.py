93f6fa53daba5321567af6700001dfc83b397657
Aditya Chopra <nytc120605@gmail.com>
2026-02-23 21:02:20 +0530

    remove backend, data, notebooks for streamlit deployment

--- backend/app/api/routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.schemas.prediction_schema import PredictionRequest
-from app.services.prediction_service import predict
-
-router = APIRouter()
-
-@router.post("/predict")
-def make_prediction(request: PredictionRequest):
-    return predict(request.dict())
--- backend/app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.api.routes import router
-
-app = FastAPI(title="ML Clinic API")
-
-app.include_router(router)
